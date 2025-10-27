ee4be89dfc375cced6a12fd63679ea1ba8802b60
alle19 <allexandra.bota@gmail.com>
2025-10-27 20:46:44 +0200

    student

--- UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/model/Student.java
+++ UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/model/Student.java
@@ -1,4 +1,36 @@
 package com.example.UniversityManagementSystem.model;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
+    private String studentId;
+    private String name;
+    private List<Enrollment> enrollments;
+
+    public Student(String studentId, String name) {
+        this.studentId = studentId;
+        this.name = name;
+        this.enrollments = new ArrayList<>();
+    }
+    public String getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<Enrollment> getEnrollments() {
+        return enrollments;
+    }
+    public void addEnrollment(Enrollment enrollment) {
+        this.enrollments.add(enrollment);
+    }
+
 }
