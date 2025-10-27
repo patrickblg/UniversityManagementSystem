0736058e3b67cbaaebb01b4782557c0a6ed8d508
alle19 <allexandra.bota@gmail.com>
2025-10-27 23:35:43 +0200

    TeachingAssignment

--- UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/model/TeachingAssignment.java
+++ UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/model/TeachingAssignment.java
@@ -1,4 +1,40 @@
 package com.example.UniversityManagementSystem.model;
 
 public class TeachingAssignment {
+    private String id;
+    private String courseId;
+    private String staffId;
+    private String managing;
+
+    public TeachingAssignment(String id, String courseId, String staffId, String managing) {
+        this.id = id;
+        this.courseId = courseId;
+        this.staffId = staffId;
+        this.managing = managing;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getCourseId() {
+        return courseId;
+    }
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+    public String getStaffId() {
+        return staffId;
+    }
+    public void setStaffId(String staffId) {
+        this.staffId = staffId;
+    }
+    public String getManaging() {
+        return managing;
+    }
+    public void setManaging(String managing) {
+        this.managing = managing;
+    }
+
 }
