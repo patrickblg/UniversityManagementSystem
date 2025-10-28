9c8890a20f4f4062615bf6992ac9da4d0459c6aa
patrickblg <blaga.patrick@yahoo.com>
2025-10-28 09:50:31 +0200

    created service files

--- UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/service/TeacherService.java
+++ UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/service/TeacherService.java
@@ -1,4 +1,26 @@
 package com.example.UniversityManagementSystem.service;
 
+import com.example.UniversityManagementSystem.model.Teacher;
+import com.example.UniversityManagementSystem.repository.TeacherRepository;
+
+import java.util.List;
+
 public class TeacherService {
+
+    private TeacherRepository teacherRepository;
+    public TeacherService(TeacherRepository teacherRepository){
+        this.teacherRepository = teacherRepository;
+    }
+    public Teacher save(Teacher teacher){
+        return teacherRepository.save(teacher);
+    }
+    public List<Teacher> findAll(){
+        return teacherRepository.findAll();
+    }
+    public Teacher findById(String id){
+        return teacherRepository.findById(id);
+    }
+    public void delete(String id){
+        teacherRepository.delete(id);
+    }
 }
