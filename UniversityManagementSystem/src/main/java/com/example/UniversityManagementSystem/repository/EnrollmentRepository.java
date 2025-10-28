0b0eded4d72d1a10ce57742017f7a00e62030af2
alle19 <allexandra.bota@gmail.com>
2025-10-28 09:43:52 +0200

    EnrollmentRepository

--- UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/repository/EnrollmentRepository.java
+++ UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/repository/EnrollmentRepository.java
@@ -5,4 +5,27 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class EnrollmentRepository {
+    private final List<Enrollment> enrollments = new ArrayList<>();
+    public Enrollment save(Enrollment e) {
+        enrollments.add(e);
+        return e;
+    }
+
+    public Enrollment findById(String id) {
+        for (Enrollment e : enrollments) {
+            if (e.getId().equals(id)) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public void delete(Enrollment e) {
+        for (int i = 0; i < enrollments.size(); i++) {
+            if (enrollments.get(i).getId().equals(e.getId())) {
+                enrollments.remove(i);
+                i--;
+            }
+        }
+    }
 }
