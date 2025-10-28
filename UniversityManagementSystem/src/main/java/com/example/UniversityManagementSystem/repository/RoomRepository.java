7614704da1d70c3f5c1e33a4cfd931fc7e24bcaa
alle19 <allexandra.bota@gmail.com>
2025-10-28 09:06:49 +0200

    StudentRepository

--- UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/repository/RoomRepository.java
+++ UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/repository/RoomRepository.java
@@ -1,4 +1,8 @@
 package com.example.UniversityManagementSystem.repository;
+import com.example.UniversityManagementSystem.model.Room;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class RoomRepository {
 }
--- UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/repository/StudentRepository.java
+++ UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/repository/StudentRepository.java
@@ -1,4 +1,38 @@
 package com.example.UniversityManagementSystem.repository;
+import com.example.UniversityManagementSystem.model.Student;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class StudentRepository {
+    private final List<Student> students = new ArrayList<>();
+    public Student save(Student s) {
+        students.add(s);
+        return s;
+    }
+    public Student findById(String id) {
+        for (Student s : students) {
+            if(s.getStudentId().equals(id)) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public List<Student> findAll() {
+        List<Student>studentsTemp = new ArrayList<>();
+        for (Student s : students) {
+            studentsTemp.add(s);
+        }
+        return studentsTemp;
+    }
+
+    public void delete(String id) {
+        for (int i = 0; i < students.size(); i++) {
+            if(students.get(i).getStudentId().equals(id)) {
+                students.remove(i);
+                i--;
+            }
+        }
+    }
 }
