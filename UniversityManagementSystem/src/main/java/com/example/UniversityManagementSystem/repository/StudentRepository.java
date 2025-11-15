8419f0613254133a6d6587f9b4a2991ae5e7ba80
alle19 <allexandra.bota@gmail.com>
2025-11-15 17:25:32 +0200

    a

--- UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/repository/StudentRepository.java
+++ UniversityManagementSystem/src/main/java/com/example/UniversityManagementSystem/repository/StudentRepository.java
@@ -2,38 +2,12 @@ package com.example.UniversityManagementSystem.repository;
 import com.example.UniversityManagementSystem.model.Student;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.ArrayList;
+
 @Repository
-public class StudentRepository implements BaseRepo<Student> {
-    private final List<Student> students = new ArrayList<>();
+public class StudentRepository extends InFileRepo<Student> {
+   public StudentRepository() {
+       super("student.json", Student.class);
+   }
 
-    @Override
-    public Student save(Student s) {
-        students.add(s);
-        return s;
-    }
-    @Override
-    public Student findById(String id) {
-        for (Student s : students) {
-            if(s.getStudentId().equals(id)) {
-                return s;
-            }
-        }
-        return null;
-    }
-    @Override
-    public List<Student> findAll() {
-        return students;
-    }
 
-    @Override
-    public void delete(String id) {
-        for (int i = 0; i < students.size(); i++) {
-            if(students.get(i).getStudentId().equals(id)) {
-                students.remove(i);
-                i--;
-            }
-        }
-    }
 }
